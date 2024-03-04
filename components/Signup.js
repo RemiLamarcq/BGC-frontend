ee427afa0fd69007354fd0e951bb661df83cad5b
Rémi <remi.lamarcq@yahoo.fr>
2024-03-04 16:50:08 +0100

    ajout du style sur signin signup

--- components/Signup.js
+++ components/Signup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, KeyboardAvoidingView } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
@@ -21,7 +21,7 @@ const handleSignUp = () => {
     console.log("Email:", email);
     console.log("Password:", password);
 
-    fetch('http://localhost:3000/users/signup', {
+    fetch('http://192.168.1.150:3000/users/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password, username }),
@@ -48,70 +48,119 @@ const handleSignUp = () => {
 
 return (
 
-<View style={styles.container}>
-  {/* Titre et logo */}
+<KeyboardAvoidingView style={styles.full} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}> 
+  <View style={styles.container}>
+    {/* Titre et logo */}
 
-  <View style={styles.logoTitle}>
+    <View style={styles.logoTitle}>
 
-    <Image
-      source={require('../assets/meeple.png')} // Assuming logo.png exists in the same directory
-      style={styles.logo}
-    />
+      <Image
+        source={require('../assets/meeple.png')} // Assuming logo.png exists in the same directory
+        style={styles.logo}
+      />
 
-    <Text style={styles.title}>Board Game Companion</Text>
+      <Text style={styles.title}>Board Game Companion</Text>
 
+    </View>
+    {/* S'inscrire */}
+
+    <View style={styles.middlePart}>
+
+      <TouchableOpacity style={styles.button} onPress={handleBackToSignin}>
+        <FontAwesome name="arrow-left" size={20} color='#000000' style={styles.deleteIcon}/>
+      </TouchableOpacity>
+
+      <TextInput
+        style={styles.input}
+        placeholder="nom d'utilisateur"
+        value={username}
+        onChangeText={(value) => setUsername(value)}
+      />
+      
+      <TextInput
+        style={styles.input}
+        placeholder="e-mail"
+        value={email}
+        onChangeText={(value) => setEmail(value)}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder=" confirmer e-mail"
+        value={confirmEmail}
+        onChangeText={(value) => setConfirmEmail(value)}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Mot de passe"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(value) => setPassword(value)}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <Text style={styles.buttonText}>S'inscrire</Text>
+      </TouchableOpacity>
+
+    </View>
   </View>
-  {/* S'inscrire */}
-
-  <View style={styles.middlePart}>
-
-    <TouchableOpacity style={styles.button} onPress={handleBackToSignin}>
-      <FontAwesome name="arrow-left" size={20} color='#000000' style={styles.deleteIcon}/>
-    </TouchableOpacity>
-
-    <TextInput
-      style={styles.input}
-      placeholder="nom d'utilisateur"
-      value={username}
-      onChangeText={(value) => setUsername(value)}
-    />
-    
-    <TextInput
-      style={styles.input}
-      placeholder="e-mail"
-      value={email}
-      onChangeText={(value) => setEmail(value)}
-    />
-
-    <TextInput
-      style={styles.input}
-      placeholder=" confirmer e-mail"
-      value={confirmEmail}
-      onChangeText={(value) => setConfirmEmail(value)}
-    />
-
-    <TextInput
-      style={styles.input}
-      placeholder="Mot de passe"
-      secureTextEntry={true}
-      value={password}
-      onChangeText={(value) => setPassword(value)}
-    />
-
-    <TouchableOpacity style={styles.button} onPress={handleSignUp}>
-      <Text style={styles.buttonText}>S'inscrire</Text>
-    </TouchableOpacity>
-
-  </View>
-</View>
+  </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  full: {
+    flex : 1,
+  },
     container: {
-    flex: 1,
+    flex: 0.5,
     backgroundColor: '#F2F4F1',
     alignItems: 'center',
     justifyContent: 'center',
     },
+    logoTitle:{
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo:{      
+      height: 150,
+      width: 150,
+    },
+    title:{
+      fontSize: 40,
+      fontWeight: '700',
+      textAlign: 'center',
+      color : '#423D3D',
+    },
+    middlePart: {
+      flex: 0.3,
+      backgroundColor:'#CDDCDB',
+      width: "70%",
+      borderRadius: 25,
+    },
+    input : {
+      backgroundColor :"white",
+      borderRadius: 50,
+      height: 40,
+      margin : 8,
+    },
+    button:{
+      backgroundColor :"#88B7B6",
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 50,
+      height: 40,
+      marginTop: 20,
+      margin : 12,
+      marginBottom: 10,
+    },
+    buttonText:{
+      textAlign: 'center',
+      fontSize: 20,
+      color :'#423D3D',
+      fontWeight: 'bold',
+    }
+  
     })
