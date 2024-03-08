514ae3282b31738edb700be688f75b693604e0a0
Thibaud Gilles <thibaud.gilles@gmail.com>
2024-03-08 11:10:07 +0100

    page stats en cours

--- screens/StatScreen.js
+++ screens/StatScreen.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, KeyboardAvoidingView, TouchableOpacity, Modal, Button } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import Header from '../components/Header';
+import formatDate from '../modules/formatDate';
+import { AutocompleteDropdownContextProvider, AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function StatScreen() {
+
+  const user = useSelector((state) => state.user.value);
+  
   return (
     <View style={styles.container}>
       <Text>Stat</Text>
