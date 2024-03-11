d9541ea4a4164a8370973bf42ca1f72b88b022ed
Laporte Andoni <lapand@hotmail.fr>
2024-03-11 18:08:59 +0100

    conflicts

--- components/AddGamePlay.js
+++ components/AddGamePlay.js
@@ -56,13 +56,7 @@ export default function AddGamePlay({toggleModalAddGamePlay}) {
         fetch(`https://bgc-backend.vercel.app/games/allNames/${token}`)
         .then(response => response.json())
         .then(data => {
-<<<<<<< HEAD
             setGameList(data.gameData);
-=======
-            const gameNames = data.gameData.map(game => game.name);         
-            setGameList(gameNames)
-
->>>>>>> 1a7d7177d4f3cb48bd7e70f617e179e5a42ae9d7
         });
     }, [addGamePlayVisible]);
 
@@ -215,8 +209,8 @@ export default function AddGamePlay({toggleModalAddGamePlay}) {
                 }),
             })
             .then(result => result.json())
-            .then(data => data.result ? toggleModalAddGamePlay() : Alert(data.error))
-            .catch(error => Alert(error));
+            .then(data => data.result ? toggleModalAddGamePlay() : Alert.alert(data.error))
+            .catch(error => Alert.alert(error));
 
         }
     }
