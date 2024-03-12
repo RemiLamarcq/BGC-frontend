05f029d4404767babe6933de8f5682c1bb33bd2e
Anna <annaka_34@yahoo.fr>
2024-03-12 16:49:15 +0100

    laure à pusher son code

--- components/Timer.js
+++ components/Timer.js
@@ -1,36 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet, ScrollView } from 'react-native';
 import Countdown from './Countdown';
 import Stopwatch from './Stopwatch';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'; // Importer FontAwesome5
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Composant enfant de AccessoiresScreen.js
-// Composant parent de Countdown.js => minuteur et Stopwatch.js => chrono
+// Clé de stockage pour AsyncStorage
+const STORAGE_KEY = 'timerState';
 
 function Timer() {
-  // État pour stocker les minuteries ajoutées
-  const [timers, setTimers] = useState([
-    { type: 'countdown', key: Date.now(), duration: 60, isCountdownActive: false },
-    { type: 'stopwatch', key: Date.now() + 1, time: 0, running: false },
-  ]);
+  // État pour stocker les minuteries
+  const [timers, setTimers] = useState([]);
 
-  // Fonction pour ajouter une minuterie de type spécifié
+  // Charger les données des minuteries sauvegardées lorsque le composant est monté
+  useEffect(() => {
+    const fetchTimers = async () => {
+      try {
+        const storedTimers = await AsyncStorage.getItem(STORAGE_KEY);
+        if (storedTimers !== null) {
+          setTimers(JSON.parse(storedTimers));
+        }
+      } catch (error) {
+        console.error('Erreur lors du chargement des minuteries depuis AsyncStorage :', error);
+      }
+    };
+  
+    fetchTimers();
+  }, []);
+
+  // Mettre à jour les données des minuteries et les sauvegarder dans AsyncStorage
+  const updateAndSaveTimers = (updatedTimers) => {
+    setTimers(updatedTimers);
+    AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedTimers))
+      .catch(error => console.error('Erreur lors de la sauvegarde des minuteries dans AsyncStorage :', error));
+  };
+
+  // Fonction pour ajouter une minuterie
   const addTimer = (type) => {
-    // Création d'une nouvelle minuterie avec des propriétés initiales en fonction du type
     const newTimer = {
       type,
-      key: Date.now(), // Utiliser une clé unique basée sur la date actuelle
+      key: Date.now(),
       name: type === 'countdown' ? `Minuteur ${timers.length + 1}` : `Chrono ${timers.length + 1}`,
       ...(type === 'countdown' ? { duration: 60, isCountdownActive: false } : { time: 0, running: false }),
     };
 
-    // Mise à jour de l'état avec la nouvelle minuterie
-    setTimers((prevTimers) => [...prevTimers, newTimer]);
+    updateAndSaveTimers([...timers, newTimer]);
   };
 
-  // Fonction pour supprimer une minuterie en fonction de sa clé unique
+  // Fonction pour supprimer une minuterie
   const removeTimer = (key) => {
-    setTimers((prevTimers) => prevTimers.filter((timer) => timer.key !== key));
+    const updatedTimers = timers.filter(timer => timer.key !== key);
+    updateAndSaveTimers(updatedTimers);
   };
 
   return (
@@ -43,26 +63,28 @@ function Timer() {
         {/* Bouton pour ajouter un minuteur */}
         <TouchableOpacity style={styles.addButton} onPress={() => addTimer('countdown')}>
           <FontAwesome5 style={styles.fabtn} name="plus-circle" size={24} color="#423D3D"/>
-          {/* Logo + de FA5 */}
+          {/* Logo + de FontAwesome5 */}
           <Text style={styles.buttonText}>Ajouter un minuteur</Text>
         </TouchableOpacity>
 
         {/* Bouton pour ajouter un chronomètre */}
         <TouchableOpacity style={styles.addButtonChrono} onPress={() => addTimer('stopwatch')}>
           <FontAwesome5 style={styles.fabtn} name="plus-circle" size={24} color="#423D3D" />
-          {/* Logo + de FA5 */}
+          {/* Logo + de FontAwesome5 */}
           <Text style={styles.buttonText}>Ajouter un chrono</Text>
         </TouchableOpacity>
       </View>
 
       {/* Utilisation de ScrollView pour permettre le défilement des minuteries */}
       <ScrollView>
+        {/* Mapper les minuteries pour les afficher */}
         {timers.map((timer) => (
           <View key={timer.key} style={styles.timerContainer}>
+            {/* Conteneur du nom de la minuterie */}
             <View style={styles.timerNameContainer}>
               <Text style={styles.timerName}>{timer.name}</Text>
             </View>
-
+            {/* Affichage de Countdown ou Stopwatch en fonction du type de la minuterie */}
             {timer.type === 'countdown' ? (
               <Countdown timer={timer} removeTimer={removeTimer} />
             ) : (
@@ -79,11 +101,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    marginTop:10,
+    marginTop: 10,
   },
   addTimer: {
     fontSize: 15,
-    color:"#423D3D",
+    color: "#423D3D",
   },
   addButtonsContainer: {
     flexDirection: 'row',
@@ -91,25 +113,25 @@ const styles = StyleSheet.create({
   },
   addButton: {
     flexDirection: 'row',
-    backgroundColor :"#88B7B6",
+    backgroundColor: "#88B7B6",
     alignItems: 'center',
     borderRadius: 50,
     height: 40,
-    width : 173,
+    width: 173,
     marginTop: 40,
     marginBottom: 20,
-    marginRight:40,
+    marginRight: 40,
   },
   addButtonChrono: {
     flexDirection: 'row',
-    backgroundColor :"#88B7B6",
+    backgroundColor: "#88B7B6",
     alignItems: 'center',
     borderRadius: 50,
     height: 40,
-    width : 173,
+    width: 173,
     marginTop: 40,
     marginBottom: 20,
-    marginRight:30,
+    marginRight: 10,
   },
   fabtn: {
     marginLeft: 5,
@@ -120,5 +142,5 @@ const styles = StyleSheet.create({
     color: "#423D3D",
   },
 });
-  
+
 export default Timer;
